112c4cd09df23e862e50e5bd499b56eace241f3d
nurislam0777 <nu_bekbolat@kbtu.kz>
2026-04-26 15:52:57 +0500

    Refactor Major class with constructor and methods
    Refactored Major class to include a parameterized constructor and implemented getter and setter methods for properties.

--- src/main/java/Model/Major.java
+++ src/main/java/Model/Major.java
@@ -1,120 +1,49 @@
-
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Major {
 
-    /**
-     * Default constructor
-     */
-    public Major() {
-    }
-
-    /**
-     * 
-     */
     private Long id;
-
-    /**
-     * 
-     */
     private String majorId;
-
-    /**
-     * 
-     */
     private String name;
-
-    /**
-     * 
-     */
     private String description;
-
-    /**
-     * 
-     */
     private School school;
 
-
-
-
-    /**
-     * @return
-     */
-    public Long getId() {
-        // TODO implement here
-        return null;
+    public Major(){
+    }
+    public Major(Long id,String majorId,String name){
+        this.id=id;
+        this.majorId=majorId;
+        this.name=name;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getMajorId(){
+        return majorId;
+    }
+    public String getName(){
+        return name;
     }
 
-    /**
-     * @return
-     */
-    public String getMajorId() {
-        // TODO implement here
-        return "";
+    public void setName(String v) {
+        this.name=v;
+    }
+    public String getDescription(){
+        return description;
     }
 
-    /**
-     * @return
-     */
-    public String getName() {
-        // TODO implement here
-        return "";
+    public void setDescription(String v){
+        this.description=v;
+    }
+    public School getSchool(){
+        return school;
+    }
+    public void setSchool(School v){
+        this.school=v;
     }
 
-    /**
-     * @param value
-     */
-    public void setName(String value) {
-        // TODO implement here
+    public String toString() {
+        return name+" "+majorId+" ";
     }
-
-    /**
-     * @return
-     */
-    public String getDescription() {
-        // TODO implement here
-        return "";
-    }
-
-    /**
-     * @param value
-     */
-    public void setDescription(String value) {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
-    public School getSchool() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param value
-     */
-    public void setSchool(School value) {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
-    public String getDescription() {
-        // TODO implement here
-        return "";
-    }
-
-    /**
-     * @param value
-     */
-    public void setDescription(String value) {
-        // TODO implement here
-    }
-
-}
+} 
