cb6ec3948ebe80b0a459b438fb08755ee14d541d
AknietBarakhan <ekwon387@gmail.com>
2026-04-26 17:21:51 +0500

    Update Dean.java

--- src/main/java/Model/Dean.java
+++ src/main/java/Model/Dean.java
@@ -6,31 +6,23 @@ import java.util.*;
  * 
  */
 public class Dean extends Manager {
-
-    /**
-     * Default constructor
-     */
+    private School school;
+    
     public Dean() {
     }
-
-    /**
-     * 
-     */
-    private School school;
 
     /**
      * @return
      */
     public School getSchool() {
-        // TODO implement here
-        return null;
+        return school;
     }
 
     /**
      * @param value
      */
     public void setSchool(School value) {
-        // TODO implement here
+        this.school = value;
     }
 
 }
