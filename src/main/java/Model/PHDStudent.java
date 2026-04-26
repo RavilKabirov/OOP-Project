f7a2cd81a70c4dc93b5254a82aaf429c553a2559
AknietBarakhan <ekwon387@gmail.com>
2026-04-26 16:51:10 +0500

    Update PHDStudent.java

--- src/main/java/Model/PHDStudent.java
+++ src/main/java/Model/PHDStudent.java
@@ -6,21 +6,24 @@ import java.util.*;
  * 
  */
 public class PHDStudent extends Student {
-
-    /**
-     * Default constructor
-     */
-    public PHDStudent() {
-    }
-
-    /**
-     * 
-     */
     private String dissertationTopic;
-
-    /**
-     * 
-     */
     private Teacher supervisor;
 
+    public PHDStudent() {}
+    
+    public String getDissertationTopic() {
+    	return  dissertationTopic;
+    }
+    
+    public void setDissertationTopic(String value) {
+    	this.dissertationTopic=value;
+    }
+    
+    public Teacher getSupervisor() {
+    	return supervisor;
+    }
+    
+    public void setSupervisor(Teacher value) {
+    	this.supervisor=value;
+    }
 }
